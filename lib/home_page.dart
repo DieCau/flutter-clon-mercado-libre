907d352a3cb11fd54df18b2fec4885410376ea71
Diego Caucota <diegocaucota@gmail.com>
2024-12-28 12:36:46 -0300

    add: body home_page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,21 +16,64 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colores.amarillo,
         elevation: 0,
         leading: IconButton(
-          icon: Image.asset('assets/imgs/menu.png'),
+          icon: Image.asset(
+            'assets/imgs/menu.png',
+            color: Colors.black54,
+            width: 30,
+          ),
           onPressed: () {},
         ),
-        title: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: TextFormField(
-            decoration: const InputDecoration(hintText: 'Buscar en Mercado Liebre'),
+        title: SizedBox(
+          height: 45,
+          child: Card(
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(0.0),
+                prefixIcon: Icon(Icons.search),
+                prefixIconColor: Colors.black38,
+                hintText: 'Buscar en Mercado Liebre',
+                hintStyle: TextStyle(color: Colors.black38),
+                border: InputBorder.none,
+              ),
+            ),
           ),
         ),
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Image.asset('assets/imgs/cart2.png'),
+            icon: Image.asset(
+              'assets/imgs/cart2.png',
+              color: Colors.black54,
+              width: 25,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/imgs/ubicacion1.png',
+                  width: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: Text('Av. Raya 500'),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 10,
+                  color: Colors.black54,
+                )
+              ],
+            ),
           )
         ],
       ),
